27393e08e6934da8df21da4aa13b9f9b7ba923c8
LPestana18 <lucaspestana0212@gmail.com>
2020-08-19 13:27:43 -0300

    SharedPreferences

--- app/src/main/java/com/example/project_motivation/ui/SplashActivity.kt
+++ app/src/main/java/com/example/project_motivation/ui/SplashActivity.kt
@@ -1,23 +1,32 @@
-package com.example.project_motivation
+package com.example.project_motivation.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.example.project_motivation.R
+import com.example.project_motivation.infra.SecurityPreferences
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var mSecurityPreferences: SecurityPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        mSecurityPreferences = SecurityPreferences(this)
 
         if(supportActionBar != null) {
             supportActionBar!!.hide()
         }
 
         buttonSave.setOnClickListener(this)
+
+        val securityPreferences = SecurityPreferences(this)
+        securityPreferences.storeString("", "")
     }
 
     override fun onClick(view: View) {
@@ -31,6 +40,8 @@ class SplashActivity : AppCompatActivity(), View.OnClickListener {
         val name = editName.text.toString()
 
         if(name != ""){
+            mSecurityPreferences.storeString("name", name)
+
             // de um jeito
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
